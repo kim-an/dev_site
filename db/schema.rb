7f019c5fbbb6852c30fce297efaf797cccf18fae
Sochin Ancheta <sochink@gmail.com>
2016-09-21 10:34:40 -0700

    deploying

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160920220540) do
+ActiveRecord::Schema.define(version: 20160919175313) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,12 +21,8 @@ ActiveRecord::Schema.define(version: 20160920220540) do
     t.integer  "width"
     t.integer  "height"
     t.integer  "campaign_id"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   add_index "ads", ["campaign_id"], name: "index_ads_on_campaign_id", using: :btree
